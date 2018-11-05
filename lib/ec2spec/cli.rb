da1f071efb2246d347687a9145223d0e8196f2a5
Koda Makoto <koda@cloudpack.jp>
2018-11-05 19:34:11 +0900

    表示用のメソッドの追加

--- lib/ec2spec/cli.rb
+++ lib/ec2spec/cli.rb
@@ -3,14 +3,14 @@ require "ec2spec"
 module EC2spec
   class CLI < Thor
 
-    HEADERS = [:type, :cpu, :memory, :network]
-
     desc "list", "Describe instances type spec"
     option :family, :desc => "Describe instance family spec"
     def list
       spec_data = EC2spec::Data.new
       spec_array = spec_data.get_all if options.empty?
       spec_array = spec_data.get_family(options[:family]) if options[:family]
+      table_array = EC2spec::Table.convert(spec_array)
+      EC2spec::Table.display(table_array)
     end
 
   end
--- lib/ec2spec/table.rb
+++ lib/ec2spec/table.rb
@@ -1,11 +1,23 @@
 module EC2spec
   class Table
     def self.convert(spec_array)
-
+      table_array = []
+      spec_array.each do |spec|
+        tmp_array = []
+        tmp_array << spec[:type]
+        tmp_array << spec[:cpu]
+        tmp_array << spec[:memory]
+        tmp_array << spec[:network]
+        table_array << tmp_array
+        table_array << :separator
+      end
+      table_array.pop
+      table_array
     end
 
     def self.display(table_array)
-
+      table = Terminal::Table.new :headings => ['Instance type', 'vCPU', 'memory(GiB)', 'networking performance'], :rows => table_array
+      puts table
     end
   end
 end
