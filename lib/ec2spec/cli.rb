c5dc4ba15555b8df0a163a72ee5a4c45ad939bc7
Koda Makoto <koda@cloudpack.jp>
2018-11-05 21:04:27 +0900

    シリーズ、世代の指定を追加

--- lib/ec2spec/cli.rb
+++ lib/ec2spec/cli.rb
@@ -6,6 +6,7 @@ module EC2spec
     desc "list", "Describe instances type spec"
     option :family, :enum => ['general', 'compute', 'memory', 'accelerated', 'storage'], :desc => "Describe instance family spec"
     option :series, :enum => ['t', 'c'], :desc => "Describe instance series spec"
+    option :generation, :type => :numeric
     def list
       begin
         spec_data = EC2spec::Data.new
@@ -13,10 +14,12 @@ module EC2spec
           spec_array = spec_data.get_all
         elsif options.has_key?('family') && options[:family] != "family"
           spec_array = spec_data.get_family(options[:family])
+        elsif (options.has_key?('series') && options[:series] != "series") && (options.has_key?('generation') && options[:generation] != "generation")
+          spec_array = spec_data.get_series_and_generation(options[:series], options[:generation])
         elsif options.has_key?('series') && options[:series] != "series"
           spec_array = spec_data.get_series(options[:series])
         end
-        raise "Can not get spec data." if spec_array.nil?
+        raise "Can not get spec data." if spec_array.nil? || spec_array.empty?
         table_array = EC2spec::Table.convert(spec_array)
         EC2spec::Table.display(table_array)
       rescue => e
--- lib/ec2spec/data.rb
+++ lib/ec2spec/data.rb
@@ -1,6 +1,13 @@
 module EC2spec
   class Data
     SPEC_DATA = [
+      {family: :general, series: :t, generation: 2, type: 't2.nano',     cpu: 1,  memory: 0.5,   network: 'Low'},
+      {family: :general, series: :t, generation: 2, type: 't2.micro',    cpu: 1,  memory: 1.0,   network: 'Low to Moderate'},
+      {family: :general, series: :t, generation: 2, type: 't2.small',    cpu: 1,  memory: 2.0,   network: 'Low to Moderate'},
+      {family: :general, series: :t, generation: 2, type: 't2.medium',   cpu: 2,  memory: 4.0,   network: 'Low to Moderate'},
+      {family: :general, series: :t, generation: 2, type: 't2.large',    cpu: 2,  memory: 8.0,   network: 'Low to Moderate'},
+      {family: :general, series: :t, generation: 2, type: 't2.xlarge',   cpu: 4,  memory: 16.0,  network: 'Moderate'},
+      {family: :general, series: :t, generation: 2, type: 't2.2xlarge',  cpu: 8,  memory: 32.0,  network: 'Moderate'},
       {family: :general, series: :t, generation: 3, type: 't3.nano',     cpu: 2,  memory: 0.5,   network: 'Low'},
       {family: :general, series: :t, generation: 3, type: 't3.micro',    cpu: 2,  memory: 1.0,   network: 'Low to Moderate'},
       {family: :general, series: :t, generation: 3, type: 't3.small',    cpu: 2,  memory: 2.0,   network: 'Low to Moderate'},
@@ -8,7 +15,6 @@ module EC2spec
       {family: :general, series: :t, generation: 3, type: 't3.large',    cpu: 2,  memory: 8.0,   network: 'Low to Moderate'},
       {family: :general, series: :t, generation: 3, type: 't3.xlarge',   cpu: 4,  memory: 16.0,  network: 'Moderate'},
       {family: :general, series: :t, generation: 3, type: 't3.2xlarge',  cpu: 8,  memory: 32.0,  network: 'Moderate'},
-      {family: :compute, series: :t, generation: 3, type: 't3.nano',     cpu: 2,  memory: 0.5,   network: 'Low'},
       {family: :compute, series: :c, generation: 5, type: 'c5.large',    cpu: 2,  memory: 4.0,   network: 'MAX 10Gbps'},
       {family: :compute, series: :c, generation: 5, type: 'c5.xlarge',   cpu: 4,  memory: 8.0,   network: 'MAX 10Gbps'},
       {family: :compute, series: :c, generation: 5, type: 'c5.2xlarge',  cpu: 8,  memory: 16.0,  network: 'MAX 10Gbps'},
@@ -41,6 +47,10 @@ module EC2spec
       when "c"
         SPEC_DATA.select {|row| row[:series] == :c}
       end
+    end
+
+    def get_series_and_generation(series, generation)
+      SPEC_DATA.select {|row| row[:series] == series.to_sym && row[:generation] == generation}
     end
   end
 end
