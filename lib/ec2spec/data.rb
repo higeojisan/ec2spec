d0df1c0bcbcccd0c3528b6b2cea1b8c6bd29314e
higeojisan <makotokoda82@gmail.com>
2018-11-06 20:08:29 +0900

    メソッドのリファクタリング

--- lib/ec2spec/data.rb
+++ lib/ec2spec/data.rb
@@ -11,29 +11,13 @@ module EC2spec
     end
 
     def get_family(family = nil)
-      case family
-      when "general"
-        @spec_data.select {|row| row[:family] == :general}
-      when "compute"
-        @spec_data.select {|row| row[:family] == :compute}
-      when "memory"
-        @spec_data.select {|row| row[:family] == :memory}
-      else
-        nil
-      end
+      return nil if family.nil?
+      @spec_data.select {|row| row[:family] == family.to_sym}
     end
 
     def get_series(series = nil)
-      case series
-      when "t"
-        @spec_data.select {|row| row[:series] == :t}
-      when "c"
-        @spec_data.select {|row| row[:series] == :c}
-      when "r"
-        @spec_data.select {|row| row[:series] == :r}
-      else
-        nil
-      end
+      return nil if series.nil?
+      @spec_data.select {|row| row[:series] == series.to_sym}
     end
 
     def get_series_and_generation(series, generation)
